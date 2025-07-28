a33dfea1caf5f09b9ba76d608fadc456b09fc1a8
Amitoj02 <40449197+Amitoj02@users.noreply.github.com>
2025-07-28 01:28:16 -0400

    feat: add login component and routing, update navbar for login link

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'login',
+  imports: [MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
--- frontend/src/app/components/navbar/navbar-mobile/navbar-mobile.component.ts
+++ frontend/src/app/components/navbar/navbar-mobile/navbar-mobile.component.ts
@@ -2,10 +2,11 @@ import {Component, inject} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {MatBottomSheetRef} from '@angular/material/bottom-sheet';
 import {MatIcon} from '@angular/material/icon';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
     selector: 'navbar-mobile',
-    imports: [MatListModule, MatIcon],
+    imports: [MatListModule, MatIcon, RouterLinkActive, RouterLink],
     templateUrl: './navbar-mobile.component.html',
     styleUrl: './navbar-mobile.component.scss'
 })
